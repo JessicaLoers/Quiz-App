dab84fd501d605fc3eb8aa38514760802ddba730
Jessica Loers <jessica.loers@gmail.com>
2021-11-25 20:09:47 +0100

    update bookmark-toggle

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,19 +1,31 @@
+let buttons = document.querySelectorAll(".question-card_button");
+let answers = document.querySelectorAll(".question-card__answer");
 
-let buttons= document.querySelectorAll('.question-card_button')
-let answers = document.querySelectorAll('.question-card__answer')
+let bookmarks = document.querySelectorAll('.card__bookmark')
 
-
-console.log(buttons)
-
-// index: array answers
-buttons.forEach((button, index) => {
-  console.log(index)
-  button.addEventListener('click', () => {
-   answers[index].classList.toggle('show')
-
-  })
-})
+// checkpoint
+console.log(buttons);
+console.log(bookmarks);
 
 
-
+buttons.forEach((button, index) => { 
   
+  console.log(index); // index: array answers
+
+  button.addEventListener("click", () => {
+    let answer = answers[index];
+    answer.classList.toggle("show");
+    if (answer.classList.contains("show")) {
+      button.innerHTML = "hide answer";
+    } else {
+      button.innerHTML = "show answer";
+    }
+  });
+});
+
+
+bookmarks.forEach((bookmark, index) => {
+  bookmark.addEventListener('click', () => {
+    bookmark.classList.toggle('bookmark-active')
+   })
+})
